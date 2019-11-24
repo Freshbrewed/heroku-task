29d1c7879ab8dbffaffa7405f9ba5b6a9492dc05
Erno Seppänen <ernseppa@gmail.com>
2019-11-24 17:42:32 +0200

    frontendin valmsitelua backendin juureen.

---
+++ build/precache-manifest.754ea720203f65a048ffd20597b12a39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aa94ee98476760a1dc72a6866804f2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b4b50acab69330862dd",
+    "url": "/static/css/main.b0f19b03.chunk.css"
+  },
+  {
+    "revision": "5e2eb85a41581a00fed6",
+    "url": "/static/js/2.be94454a.chunk.js"
+  },
+  {
+    "revision": "4b4b50acab69330862dd",
+    "url": "/static/js/main.7835997b.chunk.js"
+  },
+  {
+    "revision": "3da10799abf0dbd55beb",
+    "url": "/static/js/runtime-main.b1e82656.js"
+  }
+]);
