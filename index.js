abb285f410d5f6eaff78021a4beb28376325761a
Erno Seppänen <ernseppa@gmail.com>
2019-11-29 00:05:28 +0200

    3.18 step6 tehty.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
-app.use(bodyParser.json())
 app.use(express.static('build'))
+app.use(bodyParser.json())
 app.use(cors())
 
 morgan.token('post', postPerson = (request) => {
@@ -15,60 +17,38 @@ morgan.token('post', postPerson = (request) => {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post'))
 
 
-const generateId = () => {
-  return Math.floor(Math.random() * Math.floor(Number.MAX_VALUE))
-}
 
-let persons = [
-  { 
-     name :  "Arto Hellas", 
-     number :  "040-123456",
-     id : 1
-  },
-  { 
-     name: "Ada Lovelace", 
-     number: "39-44-5323523",
-     id : 2
-  },
-  { 
-     name: "Dan Abramov", 
-     number: "12-43-234345",
-     id: 3
-  },
-  { 
-     name: "Mary Poppendieck", 
-     number: "39-23-6423122",
-     id: 4
-  }
-]
+
 
   app.get('/', (request, response) => {
     response.send(`/info holds phonebook information</p>`)
   })
 
-  app.get('/info', (request, response) => {
-    response.send(
-      `<p>Phonebook has info for ${persons.length} people.</p>`+ 
-      `<p>${Date()}</p>` )
-      
+  app.get('/api/info', (request, response) => {
+    Person.find({}).then(persons => {
+      response.send(
+        `<p>Phonebook has info for ${persons.length} people.</p>`+ 
+        `<p>${Date()}</p>` )
+    })
+    .catch(error => next(error)) 
   })
   
   app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+      response.json(persons.map(person => person.toJSON()))
+    })
+    .catch(error => next(error))
   })
 
-  app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => {
-      return person.id === id
+  app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+    .then(person => {
+      if(person) {
+        response.json(person.toJSON())
+      }
+      response.status(404).end()
     })
-
-  if (person) {
-      console.log("Fronted request for", person)
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+    .catch(error =>  next(error))
   })
 
   app.post('/api/persons', (request, response) => {
@@ -79,33 +59,60 @@ let persons = [
         error: 'Unable to add empty person - name and number must be given.'
       })
     }
+    
+    Person.find({}).then(persons => {
+      if (persons.find(person => person.name == body.name)) {
+        return response.status(400).json({
+          error: `Data of ${body.name} already exists in database.`
+        })
+      }
+    })
 
-    //Tehtävänannossa lukee, että on estettävä lisäys, jos "lisättävä nimi on jo luettelossa".
-    //Oletetaan siis, ettei samannimisiä henkilöitä voi olla montaa tämän tehtävän puitteissa.
-    if (persons.find(person => person.name == body.name)) {
-      return response.status(400).json({
-        error: `Data of ${body.name} already exists in database.`
-      })
-    }
-
-    const person = {
+    const person = new Person({
       name: body.name,
       number: body.number,
-      id: generateId(),
+    })
+
+    person.save().then(savedPerson => {
+      response.json(savedPerson.toJSON())
+    })
+    .catch(error => next(error))
+  })
+
+  app.put('/api/persons/:id', (request, response) => {
+    const body = request.body
+
+    const person = {
+      number: body.number
     }
-  
-    persons = persons.concat(person)
-    response.json(person)
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true})
+    .then(updatedPerson => {
+      response.json(updatedPerson.toJSON())
+    })
+    .catch(error => next(error))
   })
 
   app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-    console.log(persons)
-    response.status(204).end()
+    Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
   })
+
+
+  const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+    if (error.name === 'CastError' && error.kind == 'ObjectId') {
+      return response.status(400).send({ error: 'malformatted id'})
+    }
+    next(error)
+  }
+
+  app.use(errorHandler)
   
-  const PORT = process.env.PORT ||3001
+  const PORT = process.env.PORT
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
